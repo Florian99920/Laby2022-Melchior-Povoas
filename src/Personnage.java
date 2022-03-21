91d1e000d3a4903c44288a8a595d372d862f8208
POVOAS Florian <florian_povoas@icloud.com>
2022-03-21 11:23:37 +0100

    Création de la classe Personnage avec son constructeur

---
+++ src/Personnage.java
@@ -0,0 +1,15 @@
+/**
+ * classe representant le personnage
+ */
+public class Personnage extends Position {
+
+    /**
+     * constructeur qui initialise la position du personnage
+     *
+     * @param px numéro de ligne
+     * @param py numéro de colonne
+     */
+    public Personnage(int px, int py) {
+        super(px, py);
+    }
+}

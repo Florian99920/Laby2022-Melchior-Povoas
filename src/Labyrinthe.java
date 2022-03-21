090965406f39cef7ccbcecd4f695104b0058d28b
Damien Melchior <damien.mel.2404@gmail.com>
2022-03-21 11:24:31 +0100

    Mise a jour de la classe Labyrinthe

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -3,6 +3,45 @@
  */
 class Labyrinthe{
 
+    /**
+     * tableau deux dimensions de booleens
+     * stockant les murs du labyrinthe
+     */
+    private boolean[][] murs;
+
+    /**
+     * Represente les coordonnees du personnage
+     */
+    private Personnage personnage;
+
+    /**
+     * Represente les coordonnees de la sortie
+     */
+    private Sortie sortie;
+
+    /**
+     * Represente l'affichage du labyrinthe
+     * X pour les MUR
+     * P pour le Personnage
+     * S pour la SORTIE
+     * . pour une case VIDE
+     */
+    private static final char MUR  = 'X';
+    private static final char PJ  = 'P';
+    private static final char SORTIE  = 'S';
+    private static final char VIDE  = '.';
+
+    /**
+     * Represente les actions du joueur
+     * haut pour HAUT
+     * bas pour BAS
+     * gauche pour GAUCHE
+     * droite pour DROITE
+     */
+    private static final String HAUT  = "haut";
+    private static final String BAS  = "bas";
+    private static final String GAUCHE  = "gauche";
+    private static final String DROITE  = "droite";
 
     char getChar(int x, int y) {
         throw new Error("TODO");

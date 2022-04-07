f6086ab5fccaaefc88c128fd41e977ec3ff0835f
POVOAS Florian <florian_povoas@icloud.com>
2022-04-07 18:36:17 +0200

    Ajout des methodes de tests 13 à 20 concernant les methodes deplacerPerso, toString, etreFini

--- test/TestLabyrinthe.java
+++ test/TestLabyrinthe.java
@@ -202,7 +202,6 @@ public class TestLabyrinthe {
     public void test12_getSuivantActionInconnue() throws Exception {
         // preparation des donnees
         Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
-        int[] res = new int[]{1, 4};
         boolean exception = false;
 
         // methode testee
@@ -214,5 +213,141 @@ public class TestLabyrinthe {
 
         // verification
         assertTrue(exception, "Il devrait y avoir une exception de type ActionInconnueException");
+    }
+
+    /**
+     * test de deplacerPerso pour la direction droite
+     */
+    @Test
+    public void test13_deplacerPersoDroite() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        int[] res = new int[] {2, 5};
+
+        // methode testee
+        laby.deplacerPerso(Labyrinthe.DROITE);
+
+        // verification
+        assertEquals(res[0], laby.getPersonnage().getX(),"La coordonnee en x du personnage devrait etre de 2");
+        assertEquals(res[1], laby.getPersonnage().getY(),"La coordonnee en y du personnage devrait etre de 5");
+    }
+
+    /**
+     * test de deplacerPerso pour la direction gauche
+     */
+    @Test
+    public void test14_deplacerPersoGauche() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        int[] res = new int[] {2, 1};
+
+        // methode testee
+        laby.deplacerPerso(Labyrinthe.GAUCHE);
+
+        // verification
+        assertEquals(res[0], laby.getPersonnage().getX(),"La coordonnee en x du personnage devrait etre de 2");
+        assertEquals(res[1], laby.getPersonnage().getY(),"La coordonnee en y du personnage devrait etre de 1");
+    }
+
+    /**
+     * test de deplacerPerso pour la direction haut
+     */
+    @Test
+    public void test15_deplacerPersoHaut() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        int[] res = new int[] {1, 3};
+
+        // methode testee
+        laby.deplacerPerso(Labyrinthe.HAUT);
+
+        // verification
+        assertEquals(res[0], laby.getPersonnage().getX(),"La coordonnee en x du personnage devrait etre de 1");
+        assertEquals(res[1], laby.getPersonnage().getY(),"La coordonnee en y du personnage devrait etre de 3");
+    }
+
+    /**
+     * test de deplacerPerso pour la direction bas
+     */
+    @Test
+    public void test16_deplacerPersoBas() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        int[] res = new int[] {3, 3};
+
+        // methode testee
+        laby.deplacerPerso(Labyrinthe.BAS);
+
+        // verification
+        assertEquals(res[0], laby.getPersonnage().getX(),"La coordonnee en x du personnage devrait etre de 3");
+        assertEquals(res[1], laby.getPersonnage().getY(),"La coordonnee en y du personnage devrait etre de 3");
+    }
+
+    /**
+     * test de deplacerPerso pour une action inconnue
+     */
+    @Test
+    public void test17_deplacerPersoActionInconnue() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        boolean exception = false;
+
+        // methode testee
+        try {
+            laby.deplacerPerso("fsdfsqf");
+        } catch (ActionInconnueException e) {
+            exception = true;
+        }
+
+        // verification
+        assertTrue(exception, "Il devrait y avoir une exception de type ActionInconnueException");
+    }
+
+    /**
+     * test du toString
+     */
+    @Test
+    public void test18_toString() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        String res = "XXXXXXX\nXS....X\nX..P..X\nX.....X\nXXXXXXX\n";
+
+        // methode testee
+        String affichage = laby.toString();
+
+        // verification
+        assertEquals(res, affichage,"L affichage devrait etre correct");
+    }
+
+    /**
+     * test etreFini -> Vrai
+     */
+    @Test
+    public void test19_etreFiniVrai() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+        laby.deplacerPerso(Labyrinthe.HAUT);
+        laby.deplacerPerso(Labyrinthe.GAUCHE);
+
+        // methode testee
+        boolean fini = laby.etreFini();
+
+        // verification
+        assertTrue(fini,"Le jeu devrait etre fini");
+    }
+
+    /**
+     * test etreFini -> Faux
+     */
+    @Test
+    public void test20_etreFiniFaux() throws Exception {
+        // preparation des donnees
+        Labyrinthe laby = Labyrinthe.chargerLabyrinthe("laby\\laby0.txt");
+
+        // methode testee
+        boolean fini = laby.etreFini();
+
+        // verification
+        assertTrue(!fini,"Le jeu ne devrait pas etre fini");
     }
 }
